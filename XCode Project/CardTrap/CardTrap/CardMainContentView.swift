4389f51c16361ec0b41634601fcd69d891baa1f6
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2021-09-27 20:29:16 -0400

    Created XCode Project

---
+++ XCode Project/CardTrap/CardTrap/CardMainContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CardTrap
+//
+//  Created by Colby Beach on 9/27/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        
+        Rectangle()
+            .fill(Color.red)
+            .frame(width: 200, height: 400)
+        
+        
+    }
+}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CardView()
+//    }
+//}
---
+++ XCode Project/CardTrap/CardTrap/CardTrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardTrapApp.swift
+//  CardTrap
+//
+//  Created by Colby Beach on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CardTrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CardView()
+        }
+    }
+}
